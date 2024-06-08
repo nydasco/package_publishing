19767d786b32fb9c44366f9197560d3243769f8b
Andy Sawyer <andy@nydas.co>
2024-06-08 13:22:58 +1000

    Init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # package_publishing
 A demonstration of a published package in a personal repository
+
+## Installation
+
+```bash
+pip install package_publishing
---
+++ try.py
@@ -0,0 +1,6 @@
+from package_publishing import add, subtract, multiply, divide
+
+print(add(1, 2))
+print(subtract(2, 1))
+print(multiply(2, 3))
+print(divide(6, 3))
